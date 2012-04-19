9ab1b60d4f6ec09741cfc0f76314142f9cf59c90
preble <preble@gmail.com>
2012-04-19 11:17:59 -0400

    Project reorg.

---
+++ SplitFlap/SplitFlap/SplitFlapView.h
@@ -0,0 +1,14 @@
+//
+//  SplitFlapView.h
+//  SplitFlap
+//
+//  Created by Adam Preble on 4/19/12.
+//  Copyright (c) 2012 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface SplitFlapView : GLKView
+
+@end
---
+++ SplitFlap/SplitFlap/SplitFlapView.m
@@ -0,0 +1,13 @@
+//
+//  SplitFlapView.m
+//  SplitFlap
+//
+//  Created by Adam Preble on 4/19/12.
+//  Copyright (c) 2012 Big Nerd Ranch. All rights reserved.
+//
+
+#import "SplitFlapView.h"
+
+@implementation SplitFlapView
+
+@end
